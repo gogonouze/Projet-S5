1f1e9b3c9261a683148afb699d74826cb04dc3ee
UnkneesDwark <57356970+UnkneesDwark@users.noreply.github.com>
2020-01-05 15:07:13 +0100

    Update Group.java

--- src/object/Group.java
+++ src/object/Group.java
@@ -1,5 +1,29 @@
-package object;
+import java.util.TreeSet;
 
-public class Group {
+public class Group implements Comparable<Group>{
+	private int iD_group;
+	TreeSet<User> group =new TreeSet<User>();
 
+	public int getiD_group() {
+		return iD_group;
+	}
+
+	public TreeSet<User> getGroup() {
+		return group;
+	}
+
+	@Override
+	public int compareTo(Group o2) {
+		// TODO Auto-generated method stub
+		return this.iD_group-o2.iD_group;
+	}
+	
+	public boolean equals (Object obj) {
+		if(obj!=null && obj instanceof Group) {
+			Group other=(Group) obj;
+			return this.iD_group==other.iD_group;
+			
+		}
+		return false;
+	}
 }
