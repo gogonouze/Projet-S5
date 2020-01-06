11090ee549f7109488a56f9bcf732ca515e037aa
UnkneesDwark <57356970+UnkneesDwark@users.noreply.github.com>
2020-01-06 22:06:32 +0100

    Wololo c'est bo

--- src/object/Client.java
+++ src/object/Client.java
@@ -2,8 +2,10 @@ package object;
 
 public class Client extends User {
 
-	public Client(String name) {
-		super(name);
+	public Client(String name,Port_Pool p) {
+		super(name,p);
+		// TODO Auto-generated constructor stub
 	}
 
 }
+
