55684c6a6869044ad6b167eaed514cebcb07eed7
gogonouze <47778065+gogonouze@users.noreply.github.com>
2020-01-08 21:51:57 +0100

    aled

---
+++ src/gui_element/PopUp_NewDiscussion.java
@@ -0,0 +1,102 @@
+package gui_element;
+
+import object.User;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Window.Type;
+import java.awt.Dialog.ModalityType;
+
+public class PopUp_NewDiscussion extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JComboBox comboBox;
+	private JTextArea textArea;
+	
+	private User user;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void launch(User user) {
+		try {
+			PopUp_NewDiscussion dialog = new PopUp_NewDiscussion(user);
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public PopUp_NewDiscussion(User user) {
+		this.user = user;
+		
+		setAlwaysOnTop(true);
+		setType(Type.POPUP);
+		setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.NORTH);
+			{
+				JLabel lblPickAGroup = new JLabel("Pick a group");
+				lblPickAGroup.setHorizontalAlignment(SwingConstants.LEFT);
+				panel.add(lblPickAGroup);
+			}
+			{
+				comboBox = new JComboBox();
+				panel.add(comboBox);
+			}
+		}
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			contentPanel.add(scrollPane, BorderLayout.CENTER);
+			{
+				textArea = new JTextArea();
+				scrollPane.setViewportView(textArea);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Send");
+				okButton.setEnabled(false);
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
