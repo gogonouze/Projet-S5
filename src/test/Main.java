7b34c325fc648d65a5dd43deae20fa662a5846ae
UnkneesDwark <57356970+UnkneesDwark@users.noreply.github.com>
2020-01-08 23:10:14 +0100

    noooo

--- src/test/Main.java
+++ src/test/Main.java
@@ -8,7 +8,11 @@ import object.User;
 public class Main {
 
 	public static void main(String[] args) {
-	
+		User gerard = new Client("gerard");
+		User arouf = new Client("Arouf");
+		gerard.create_account(gerard.getNameUser(), "NarutoRunner");
+		arouf.create_account(arouf.getNameUser(),"Gangsta");
+		gerard.
 	}
 
 }
