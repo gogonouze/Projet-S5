f9d98d7eda3013b93119d2bc8e9508e44b44bc73
UnkneesDwark <57356970+UnkneesDwark@users.noreply.github.com>
2020-01-08 23:12:45 +0100

    Update Main.java

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,16 +3,23 @@ package test;
 import object.Client;
 import object.Discussion;
 import object.Gui;
+import object.Server;
 import object.User;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Server c = new Server();
+		Thread t = new Thread(c);
+		t.start();
 		User gerard = new Client("gerard");
 		User arouf = new Client("Arouf");
 		gerard.create_account(gerard.getNameUser(), "NarutoRunner");
 		arouf.create_account(arouf.getNameUser(),"Gangsta");
-		gerard.
+		gerard.createGroup("alo");
+		arouf.requestGroup();
+		arouf.createConversation("Fan d'arouf?", "Viva Arouf", arouf.getAllGroup().get(0));
+		System.out.println(gerard.getDiscussions().first().getMessages().first().getMessage());
 	}
 
 }
